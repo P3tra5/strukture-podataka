ae8d33e824de14535ea43bb99b94ef095556191a
P3tra5 <115412438+P3tra5@users.noreply.github.com>
2022-10-29 18:40:33 +0200

    Update zad21.c

--- vj3/zad21.c
+++ vj3/zad21.c
@@ -13,6 +13,7 @@
 #define DODAJ_IZA (8)
 #define DODAJ_ISPRED (9)
 #define SORTIRAJ_LISTU (10)
+#define BRISI_SVE (11)
 #define USPJESNO_IZVRSENO (15)
 #define NEUSPJESNO_IZVRSENO (16)
 #define NIJE_PRONADEN (99)
@@ -52,7 +53,7 @@ int main(void) {
 		int naredba = 0;
 		printf("Unesite broj za naredbu.\n1-Unos na pocetak liste:\n2-Unos na kraj liste\n3-Ispis liste:\n");
 		printf("4-Nadi element prema imenu:\n7-Izbrisi element:\n8-Dodaj iza odredenog elementa\n");
-		printf("9-Dodaj ispred elementa prema imenu:\n10-Sortiraj listu prema prezimenu:\n");
+		printf("9-Dodaj ispred elementa prema imenu:\n10-Sortiraj listu prema prezimenu:\n11-Izbrisi cijelu listu\n");
 
 		scanf("%d", &naredba);
 
@@ -162,6 +163,14 @@ int main(void) {
 
 		}
 
+		else if (naredba == BRISI_SVE) {
+		if (BrisiSve(&Head) == USPJESNO_IZVRSENO) {
+			printf("Memorija ociscena\n");
+		}
+		else printf("Memorija nije ociscena!\n");
+
+		}
+		
 		else {
 			printf("Greska pri unosu!\n");
 		}
@@ -171,11 +180,11 @@ int main(void) {
 		scanf("%d", &rad_programa);
 		
 	}
-	if (BrisiSve(&Head) == USPJESNO_IZVRSENO) {
+	/*if (BrisiSve(&Head) == USPJESNO_IZVRSENO) {
 		printf("Memorija ociscena");
 	}
 	else printf("Memorija nije ociscena");
-	return USPJESNO_ZAVRSEN_PROGRAM;
+	return USPJESNO_ZAVRSEN_PROGRAM;*/	//brisat?
 }
 
 int NaPocetak(Pozicija* Head_Next, osoba Unesena_Osoba) {
@@ -300,10 +309,17 @@ int SortirajListu(Pozicija P) {
 	return USPJESNO_IZVRSENO;
 }
 
-int BrisiSve(Pozicija Head) {
-
-
-
+int BrisiSve(Pozicija Head) {	//ne radi...
+	Pozicija i;
+	i = NULL;
+	
+	while (Head != NULL) {
+		i = Head;
+		Head = Head->next;
+	
+		free(i);
+	}
+	return USPJESNO_IZVRSENO;
 }
 
 //Dodat slucaj ako ne postoji iza ili ispred koga se dodaje
