ea8159f12ded627789c1570e615ffa7e258e9f5b
P3tra5 <115412438+P3tra5@users.noreply.github.com>
2022-12-20 18:26:09 +0100

    Add files via upload

--- zad8.c
+++ zad8.c
@@ -5,6 +5,14 @@ Treba omoguæiti unošenje novog elementa u stablo,ispis elemenata(inorder, preor
 #include <string.h>
 
 enum ERROR_CODE {
+	DODAJ_EL = 1,
+	ISPIS_INORDER,
+	ISPIS_PREORDER,
+	ISPIS_POSTORDER,
+	ISPIS_LEVELORDER,
+	BRISI_EL,
+	TRAZI_EL,
+	EXIT,
 	USPJESNO_ZAVRSEN_PROGRAM,
 	USPJESNO_IZVRSENO,
 	NEUSPJESNO_IZVRSENO,
@@ -16,50 +24,109 @@ typedef struct cvorStabla {
 	int El;
 	Pozicija L;
 	Pozicija D;
-}Stablo;
+}cvorStabla;
 
-Pozicija StvoriPrazno(Pozicija); //
+Pozicija StvoriPrazno(int);
 Pozicija TraziMin(Pozicija);
 Pozicija TraziMax(Pozicija);
 Pozicija Trazi(int, Pozicija);
 Pozicija DodajEl(int, Pozicija);
-Pozicija BrisiEl(int, Pozicija);
+Pozicija BrisiEl(Pozicija, int);
 int IspisInorder(Pozicija);
 int IspisPreorder(Pozicija);
 int IspisPostorder(Pozicija);
 int IspisLevelOrder(Pozicija);
+int Visina(Pozicija);
+int IspisTrenutniLevel(Pozicija, int);
 
 
 
 int main(void) {
 
 	Pozicija root = NULL;
+
 	root = DodajEl(4, root);
-	root = DodajEl(1, root);
-	root = DodajEl(12, root);
-	root = DodajEl(10, root);
-	root = DodajEl(3, root);
-	root = DodajEl(14, root);
-	root = DodajEl(2, root);
+	DodajEl(1, root);
+	DodajEl(12, root);
+	DodajEl(10, root);
+	DodajEl(3, root);
+	DodajEl(14, root);
+	DodajEl(2, root);
 
 	int naredba = 0;
-	printf(
-		"Unesite broj za naredbu.\n"
-		"_________________________\n"
-		"1-Dodaj element:\n"
-		"2-Ispis elemenata inorder\n"
-		"3-Ispis elemenata preorder:\n"
-		"4-Ispis elemenata postorder:\n"
-		"5-Ispis elemenata level order:\n"
-		"6-Brisanje elementa\n"
-		"7-Trazenje elementa\n"
-	);
+	int br = 0;
+
+	do {
+			printf(
+				"\n__________________________\n"
+				"Unesite broj za naredbu.\n"
+				"__________________________\n"
+				"1-Dodaj element\n"
+				"2-Ispis elemenata inorder\n"
+				"3-Ispis elemenata preorder\n"
+				"4-Ispis elemenata postorder\n"
+				"5-Ispis elemenata level order\n"
+				"6-Brisanje elementa\n"
+				"7-Trazenje elementa\n"
+				"8-Zavrsi program\n"
+			);
+			scanf(" %d", &naredba);
+
+		if (naredba == DODAJ_EL) {
+			printf("Koji element zelis dodati u stabla?\n");
+			scanf(" %d", &br);
+			root = DodajEl(br, root);
+		}
+		else if (naredba == ISPIS_INORDER) {
+			IspisInorder(root);
+		}
+		else if (naredba == ISPIS_PREORDER) {
+			IspisPreorder(root);
+		}
+		else if (naredba == ISPIS_POSTORDER) {
+			IspisPostorder(root);
+		}
+		else if (naredba == ISPIS_LEVELORDER) {
+			IspisLevelOrder(root);
+		}
+		else if (naredba == BRISI_EL) {
+			
+			printf("Koji element zelis izbrisati iz stabla?\n");
+			scanf(" %d", &br);
+			root = BrisiEl(root, br);
+		}
+		else if (naredba == TRAZI_EL) {
+			Pozicija P = NULL;
+			printf("Koji element trazis u stablu?\n");
+			scanf(" %d", &br);
+			P = Trazi(br, root);
+			if (P == NULL) {
+				printf("Nema elementa\n");
+			} else 
+			printf("Element %d je na poziciji %d\n", br, P);
+		}
+		else if (naredba != EXIT) {
+			printf("Nepoznata naredba\n");
+		}
+
+	} while (naredba != EXIT);
 
 	return USPJESNO_ZAVRSEN_PROGRAM;
 }
 
-Pozicija StvoriPrazno(Pozicija p) {
+Pozicija StvoriPrazno(int x) {
 
+	Pozicija Pozicija_Nova = (Pozicija)malloc(sizeof(cvorStabla));
+
+	if (Pozicija_Nova == NULL) {
+		printf("Memorija nije alocirana!\n");
+		return NEUSPJESNO_IZVRSENO;
+	}
+	Pozicija_Nova->El = x;
+	Pozicija_Nova->L = NULL;
+	Pozicija_Nova->D = NULL;
+
+	return Pozicija_Nova;
 }
 
 Pozicija TraziMin(Pozicija p) {
@@ -75,17 +142,18 @@ Pozicija TraziMax(Pozicija p) {
 
 Pozicija Trazi(int x, Pozicija p) {
 	
-	if (p == NULL) {
-		return NULL;
-	}
+	while (p != NULL) {
 
-	if (p->El < x) {
-		p->D = Trazi(x, p->D);
+		if (p->El < x) {
+			p = p->D;
+		}
+		else if (p->El > x) {
+			p = p->L;
+		}
+		else
+			return p;
 	}
-	else if (p->El > x) {
-		p->L = Trazi(x, p->L);
-	}
-	return p;
+	return NULL;
 }
 
 Pozicija DodajEl(int x, Pozicija p) {
@@ -103,7 +171,7 @@ Pozicija DodajEl(int x, Pozicija p) {
 	return p;
 }
 
-Pozicija BrisiEl(int x, Pozicija p) {
+Pozicija BrisiEl(Pozicija p, int x) {
 	
 	if (p == NULL) {
 		return NULL;
@@ -126,6 +194,10 @@ Pozicija BrisiEl(int x, Pozicija p) {
 			p->El = temp->El;
 			p->D = BrisiEl(p->D, temp->El);
 		}
+		else {
+			free(p);
+			return NULL;
+		}
 	}
 	return p;
 
@@ -136,9 +208,9 @@ int IspisInorder(Pozicija p) {
 	if (p == NULL) {
 		return 0;
 	}
-	print(p->L);
-	printf(" %s", p->El);
-	print(p->D);
+	IspisInorder(p->L);
+	printf(" %d", p->El);
+	IspisInorder(p->D);
 
 	return USPJESNO_IZVRSENO;
 }
@@ -148,9 +220,9 @@ int IspisPreorder(Pozicija p) {
 	if (p == NULL) {
 		return 0;
 	}
-	printf(" %s", p->El);
-	print(p->L);
-	print(p->D);
+	printf(" %d", p->El);
+	IspisPreorder(p->L);
+	IspisPreorder(p->D);
 
 	return USPJESNO_IZVRSENO;
 }
@@ -160,14 +232,52 @@ int IspisPostorder(Pozicija p) {
 	if (p == NULL) {
 		return 0;
 	}
-	print(p->L);
-	print(p->D);
-	printf(" %s", p->El);
+	IspisPostorder(p->L);
+	IspisPostorder(p->D);
+	printf(" %d", p->El);
 
 	return USPJESNO_IZVRSENO;
 }
 
 int IspisLevelOrder(Pozicija p) {
+
+	if (p == NULL) {
+		return 0;
+	}
+
+	int h = Visina(p);
+	for (int i = 0; i < h; i++)
+		IspisTrenutniLevel(p, i);
+
+	return USPJESNO_IZVRSENO;
+}
+
+int Visina(Pozicija p) {
+
+	if (p == NULL)
+		return 0;
+	else {
+		int LVisina = Visina(p->L);
+		int RVisina = Visina(p->D);
+
+		if (LVisina > RVisina)
+			return (LVisina + 1);
+		else
+			return (RVisina + 1);
+	}
+}
+
+int IspisTrenutniLevel(Pozicija p, int lvl) {
+	
+	if (p == NULL)
+		return 0;
+
+	if (lvl == 1)
+		printf(" %d ", p->El);
+	else if (lvl > 1) {
+		IspisTrenutniLevel(p->L, lvl - 1);
+		IspisTrenutniLevel(p->D, lvl - 1);
+	}
 
 	return USPJESNO_IZVRSENO;
 }
