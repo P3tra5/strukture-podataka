3b4210828cb8fba8a7cf0626e98cb92ee69f6b4e
P3tra5 <115412438+P3tra5@users.noreply.github.com>
2023-01-15 19:35:52 +0100

    Update kod10.c

--- Vjezba10/kod10.c
+++ Vjezba10/kod10.c
@@ -27,6 +27,7 @@ typedef struct Drzava {
 	P_Grad root;
 }Drzava;
 
+int CitaDatDrzave(P_Drzava head);
 int NovaDrzava(char, char, P_Drzava);
 
 int Unos_Gradova(P_Drzava, char);
@@ -35,11 +36,16 @@ int NoviGrad(char, int, P_Grad);
 
 
 int main(void) {
-	char buffer[MAX_LINE];
+	
+	return 0;
+}
+
+int CitaDatDrzave(P_Drzava head){
 	FILE* fp=NULL;
+	char buffer[MAX_LINE] = { 0 };
 	char* p = buffer;
-	char drzava[MAX_LINE];
-	char file[MAX_LINE];
+	char drzava[MAX_LINE] = { 0 };
+	char file[MAX_LINE] = { 0 };
 	fp = fopen("drzave.txt", "r");
 	if (fp == NULL) {
 		printf("Dokument se nije otvorio.\n");
@@ -75,9 +81,6 @@ int NovaDrzava(char naziv, char file, P_Drzava Head) {
 	}
 	strcpy(Nova_Drzava->Naziv_Drzava, naziv);
 
-	if (Head->next == NULL) {
-		Head->next = Nova_Drzava;
-	}
 	while (Head->next != NULL && strcmp(Head->next->Naziv_Drzava, Nova_Drzava->Naziv_Drzava) < 0) {
 		Head = Head->next;
 	}
@@ -91,31 +94,58 @@ int NovaDrzava(char naziv, char file, P_Drzava Head) {
 }
 
 int Unos_Gradova(P_Drzava Nova_Drzava, char file) {
-	
-	
-	char buffer[MAX_LINE];
 	FILE* fp = NULL;
+	char buffer[MAX_LINE] = { 0 };
 	char* p = buffer;
-	char drzava[MAX_LINE];
-	char file[MAX_LINE];
+	char drzava[MAX_LINE] = { 0 };
+	char file[MAX_LINE] = { 0 };
+	char naziv_grada[MAX_LINE]  = { 0 };
+	int broj_stanovnika = 0;
+	
 	fp = fopen(file, "r");
 	if (fp == NULL) {
 		printf("Dokument se nije otvorio.\n");
 		return FILE_NOT_OPEN;
 	}
-
-	char naziv_grada[MAX_LINE];
-	int broj_stanovnika;
 	while (!feof(fp)) {
 		fgets(buffer, MAX_LINE, fp);
-		sscanf(p, " %s %d", naziv_grada, broj_stanovnika);
+		sscanf(p, " %s %d", naziv_grada, &broj_stanovnika);
 		NoviGrad(naziv_grada,broj_stanovnika,Nova_Drzava->root);
 	}
 
-
+	return USPJESNO_IZVRSENO;
 
 }
-int NoviGrad(naziv_grada, broj_stanovnika, Nova_Drzava->root) {
 
+int NoviGrad(char Naziv_Grada, int broj_stanovnika, Nova_Drzava->root) {
+	P_Grad Novi_Grad = NULL;
+	
+	P_Grad Novi_Grad = malloc(sizeof(Grad));
+	Novi_Grad->L = NULL;
+	Novi_Grad->D = NULL;
+	Novi_Grad->broj_stanovnika = 0;
+	if (Nova_Drzava == NULL) {
+		printf("Memorija nije alocirana.");
+		return NEUSPJESNO_IZVRSENO;
+	}
+	strcpy(Novi_Grad->Naziv_Grad, Naziv_Grad);
+	
+	return USPJESNO_IZVRSENO;
+}
 
+int UnosGrada(P_Grad root, P_Grad Novi_Grad){
+	if(root == NULL)
+		root = Novi_Grad;
+	else if(Novi_Grad->broj_stanovnika > root->broj_stanovnika)
+		root->D = UnosGrad(root->D, Novi_Grad);
+	else if(Novi_Grad->broj_stanovnika < root->broj_stanovnika)
+		root->L = UnosGrad(root->L, Novi_Grad);
+	else{
+		if(strcmp(Novi_Grad->Naziv_Grad, root->Naziv_Grad) > 0)
+			root->D = UnosGrad(root->D, Novi_Grad);
+		else if(strcmp(Novi_Grad->Naziv_Grad, root->Naziv_Grad) < 0)
+			root->L = UnosGrad(root->L, Novi_Grad);
+	}
+	
+	return USPJESNO_IZVRSENO;
 }
